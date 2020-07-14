b82f370b50e46ec0ec1af99cbc8d835b8a58e332
DUMBELL <kritsakornsamakul61@gmail.com>
2020-07-14 15:37:33 +0700

    initial commit

---
+++ ComProSec1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "1st Term of 2020\t" << "/" << "45132114" << "\tKrirsakorn Samakul" << endl;
+	cout << endl << "-------------------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject name\t" << "\t\t| Score (100) | Grad |" << endl;
+	cout << "-------------------------------------------------------------------------" << endl;
+	cout << "|   4570034  | Introduction to computer\t" << "\t|     85      |  'A'  |" << endl;
+	cout << "|   4580247  | English                 \t" << "\t|     74      |  'B'  |" << endl;
+	cout << "|   4530001  | Design Thinking         \t" << "\t|     92      |  'A'  |" << endl;
+	cout << "-------------------------------------------------------------------------" << endl;
+	cout << "|   TOTAL    | Subject = 3             \t" << "\t|   Average = 3.87    |" << endl;
+	cout << "=========================================================================" << endl;
+	cout << endl;
+	cout << "KMUTNB" << endl;
+
+}
